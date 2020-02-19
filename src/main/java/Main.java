f110ffc471724c9317bb4661aca90556ae22595c
Jacob bishop <jbishop@bsu.edu>
2020-02-19 16:14:22 -0500

    Testing pull requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main { // hi maxx
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
